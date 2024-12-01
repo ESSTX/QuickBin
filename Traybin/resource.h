3acc0df6810c880626c52f8dcc8e1cd4cd37ae8e
esstx <yayaya@yayayayayayayaya.yaya>
2024-12-01 08:36:04 +0500

    Update resource.h

--- Traybin/resource.h
+++ Traybin/resource.h
@@ -1,15 +1,9 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Resource.rc
-//
-#define IDI_EMPTY_ICON                  106
-#define IDI_FULL_ICON                   107
-
-// Next default values for new objects
-// 
+#define IDI_MAIN                        101
+#define IDI_EMPTY_ICON                  108
+#define IDI_FULL_ICON                   110
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_RESOURCE_VALUE        114
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
